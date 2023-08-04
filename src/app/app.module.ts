7dc2e5f3c74ce95ca263bf0b29b4f372e31a64da
Pham Ngoc Lam <plam15884@gmail.com>
2023-08-04 09:48:24 +0700

    deploy len firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { CatFactEffects } from 'src/ngrx/effects/catFact.effect';
 import { catFactReducer } from 'src/ngrx/reducers/catFact.reducer';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent, CardComponent],
@@ -18,6 +22,9 @@ import { catFactReducer } from 'src/ngrx/reducers/catFact.reducer';
     HttpClientModule,
     StoreModule.forRoot({ catFact: catFactReducer }, {}),
     EffectsModule.forRoot([CatFactEffects]),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'webbanhang-74a05',
+    appId: '1:114702580915:web:06b08977faacdd5d9744c6',
+    storageBucket: 'webbanhang-74a05.appspot.com',
+    apiKey: 'AIzaSyCpdgJD4-AxRl5GHYGbJ0293z6c1Q3Tpho',
+    authDomain: 'webbanhang-74a05.firebaseapp.com',
+    messagingSenderId: '114702580915',
+    measurementId: 'G-YG9T4Q2LLB',
+  },
+};
